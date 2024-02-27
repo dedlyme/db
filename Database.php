7e5202a415cb9c1ab899bd4a49b244f42c237b13
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-27 14:38:55 +0200

    sdsdsd

---
+++ Database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database {
+
+private $pdo;
+
+public function __construct()
+{
+$connection_string = "mysql:host=localhost;dbname=blog;user=root;password=;charset=utf8mb4";
+$this->pdo = new PDO($connection_string);
+$this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}
+
+public function execute($query_string){
+    
+$query = $this->pdo->prepare($query_string);
+
+$query->execute();
+
+return $query;
+    }
+}
--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
 require "function.php";
-echo "Hi <br>";
+require "Database.php";
 
-$connection_string = "mysql:host=localhost;dbname=blog;user=root;password=;charset=utf8mb4";
-$pdo = new PDO($connection_string);
-
-
-$query = $pdo->prepare("SELECT * FROM posts");
-
-$query->execute();
-
-$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database();
+$posts = $db
+    ->execute("SELECT * FROM posts")
+    ->fetchAll();
 
 echo "<ul>";
 foreach($posts as $post) {
