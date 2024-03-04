16258e144db5f7adcd4d642b3b636813505a2550
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-04 15:19:46 +0200

    dsdsdsdsd

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
 require "function.php";
 require "Database.php";
+
 $config = require("config.php");
+
+
+$query = "SELECT * FROM posts";
+if (isset($_GET["id"]) && $_GET["id"] != ""){
+    $id = $_GET["id"];
+    $query = "SELECT * FROM posts WHERE id=$id";
+}
 $db = new Database($config);
 $posts = $db
-    ->execute("SELECT * FROM posts")
+    ->execute($query)
     ->fetchAll();
 
+echo "<form>";
+echo "<input name='id' />";
+echo "<button>Submit></button>";
+echo"</form>";
+
+
+
+echo "<h1>posts</h1>";
 echo "<ul>";
 foreach($posts as $post) {
     echo "<li>" . $post["title"] . "</li>";
