2461ed1af9c6680d688d58e051277af81e955add
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-15 09:05:06 +0200

    router.php and routes.php added

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
 
 require "functions.php";
-$url_array = parse_url($_SERVER["REQUEST_URI"]);
-$url = $url_array["path"];
 
-if ($url == "/"){
-    require "controllers/index.php";
-} else if ($url == "/story"){
-    require "controllers/story.php";
-} else if ($url == "/about"){
-    require "controllers/about.php";
-} else {
-    http_response_code(404);
-    require "controllers/404.php";
-}
+// echo "cau";
+
+// $data = [
+//     "name" => "Railijs",
+//     "age" => 6.5,
+//     "height" => "5'5",
+//     "gender" => "toster"
+// ];
+// $data2 = [
+//     "name" => "Didzis",
+//     "height" => "6'9"
+// ];
+//  ?? "lohs tev nav dzives";
+
+// if (array_key_exists("age, $data2")){
+//     echo $data2["age"]
+// }
+
+require "router.php";
+
---
+++ routes.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    "/" => "controllers/index.php",
+    "/about" => "controllers/about.php",
+    "/story" => "controllers/story.php"
+];
